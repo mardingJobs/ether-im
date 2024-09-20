ceee8bcde789d33500abf93512a204ea4b89caa2
martin <14334614+martin-jobs@user.noreply.gitee.com>
2024-09-20 13:45:59 +0800

    ```添加Nacos发现服务和负载均衡配置
    在ether-im-gateway、ether-im-message和ether-im-push模块中，新增了Nacos发现服务配置。此外，gateway模块还配置了基于Nacos的路由，以实现不同服务的负载均衡。相关依赖和属性也在pom.xml中进行了更新。
    ```

---
+++ ether-im-gateway/src/main/java/cn/ether/im/gateway/EtherImGateway.java
@@ -0,0 +1,19 @@
+package cn.ether.im.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * * @Author: Martin(微信：martin-jobs)
+ * * @Date    2024/9/20 13:41
+ * * @Description
+ * * @Github <a href="https://github.com/mardingJobs">Github链接</a>
+ **/
+@SpringBootApplication
+public class EtherImGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtherImGateway.class, args);
+    }
+
+}
